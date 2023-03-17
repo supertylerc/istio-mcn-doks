d1c6ea0d2587bf553851b7d7104bc5aad64f74be
Tyler Christiansen <code@tylerc.me>
2023-03-16 20:26:35 -0400

    formatting

--- modules/doks/providers.tf
+++ modules/doks/providers.tf
@@ -2,17 +2,17 @@ terraform {
   required_version = ">= 1.4"
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = ">= 2.0"
     }
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = ">= 4.57"
     }
   }
 }
 
 provider "google" {
-  project     = var.gcp_project
-  region      = var.gcp_region
+  project = var.gcp_project
+  region  = var.gcp_region
 }
